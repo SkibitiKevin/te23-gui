9a351f6f722ae7f127d6a7a99a2b2432a41e7c3b
Kevin <kevin.johansson5@elev.ga.ntig.se>
2025-04-02 09:58:32 +0200

    Vecka 15 Clicker app.py

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from tkinter import *
 
-click_currency = 0
+uppgradering = [
+    {
+        "name": "Våffel Järn"
+    },
+    {
 
+    }
+]
+uppgradering.append('Click +1') #uppgradering = ['Klick +1']
 
 root = Tk()
 root.title("Mitt program")
-textBox = Label(root, text="Waffffleeeeeeeeees")
+textBox = Label(root, text="Waffle Clicker TM")
 textBox.pack()
 
-def show():
-    click_currency =+ 1
-    textBox.config(text=f"{click_currency}")
+click_currency = 0
+w = Label(root, text=click_currency)
+w.pack()
 
-show_button = Button(root, text=click_currency, width=50, command=show)
+
+def show():
+    global click_currency
+    click_currency += 1
+    # textBox.config(text=f"{click_currency}")
+    w.config(text=click_currency)
+
+def show_upgrades():
+    uppgradering.pop
+    
+
+show_button = Button(root, text="Klick för att tjäna", width=50, command=show)
 show_button.pack()
 
 button = Button(root, text="Quit program", width=50, command=root.destroy)
